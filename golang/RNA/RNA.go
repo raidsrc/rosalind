795e5c7b738c4db4bb6a421d2eea095c919e922f
Ray Legion <ray.chen@virovek.com>
2026-04-17 11:32:41 -0700

    first-ish commit. getting through the first handful of problems in go to familiarize myself with the syntax and quirks of the language.

---
+++ golang/RNA/RNA.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	// "log"
+)
+
+// convert T's into U's
+
+func main() {
+	filepath := "rosalind_rna.txt"
+	content, err := os.ReadFile(filepath)
+	if err != nil {
+		// log.Fatal(err)
+		panic(err)
+	}
+
+	dnaString := string(content)
+	rnaString := strings.ReplaceAll(dnaString, "T", "U")
+	fmt.Println(rnaString)
+
+}
